4ea30dd8714b3530331f5ea18935bfd2dc26d92a
Richard <richardsvicka6@gmail.com>
2026-06-08 19:55:21 +0200

    Switch to cads-api-client library

--- backend/fetch_cds.py
+++ backend/fetch_cds.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 """
 Stiahne ECMWF dáta z Copernicus CDS pre všetky lokality.
-Používa CDS API - potrebný API key.
+Používa CADS API Client - nová verzia.
 """
 
 import json
 import os
 from datetime import datetime
-from cdsapi import Client
+from cads_api_client import ApiClient
 
 # Načítaj lokality
 with open(os.path.join(os.path.dirname(__file__), 'locations.json')) as f:
     locations = json.load(f)['locations']
 
-# CDS API klient (číta konfiguráciu z ~/.cdsapirc)
-c = Client()
+# CDS API klient
+api_key = os.environ.get('CDS_API_KEY', 'e1f7a29a-8a37-494c-86ff-8795fea9b426')
+client = ApiClient(url='https://cds.climate.copernicus.eu/api', key=api_key)
 
 all_data = {}
 
@@ -23,27 +24,27 @@ for loc in locations:
     
     # ECMWF predpoveď z CDS - Open Data API
     target_date = datetime.utcnow().strftime('%Y-%m-%d')
-    c.retrieve(
-        'ecmwf-open-data',
-        {
-            'date': target_date,
-            'time': '00:00',
-            'step': '0/to/240/by/6',
-            'type': 'fc',
-            'levtype': 'sfc',
-            'params': [
-                '2t',      # 2m teplota
-                '10u',     # 10m vietor U
-                '10v',     # 10m vietor V
-                'msl',     # tlak
-                'tp',      # zrážky
-                'tcc',     # oblačnosť
-            ],
-            'area': [loc['lat']+0.5, loc['lon']-0.5, loc['lat']-0.5, loc['lon']+0.5],
-            'format': 'grib',
-        },
-        f'/tmp/{loc["name"]}.grib'
-    )
+    
+    request = {
+        'date': target_date,
+        'time': '00:00',
+        'step': '0/to/240/by/6',
+        'type': 'fc',
+        'levtype': 'sfc',
+        'params': [
+            '2t',      # 2m teplota
+            '10u',     # 10m vietor U
+            '10v',     # 10m vietor V
+            'msl',     # tlak
+            'tp',      # zrážky
+            'tcc',     # oblačnosť
+        ],
+        'area': [loc['lat']+0.5, loc['lon']-0.5, loc['lat']-0.5, loc['lon']+0.5],
+        'format': 'grib',
+    }
+    
+    # Stiahni dáta
+    client.retrieve('ecmwf-open-data', request, f'/tmp/{loc["name"]}.grib')
     
     all_data[loc['name']] = {
         'lat': loc['lat'],
